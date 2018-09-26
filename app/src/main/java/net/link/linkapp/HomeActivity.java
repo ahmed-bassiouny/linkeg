a84584f85f23c7aa9abf0f41b9b8c4b8b06b3195
admin <ahmedbassiouny26@gmail.com>
2018-09-27 00:10:04 +0200

    add web view

--- app/src/main/java/net/link/linkapp/HomeActivity.java
+++ app/src/main/java/net/link/linkapp/HomeActivity.java
@@ -6,36 +6,16 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.widget.ProgressBar;
 
 import bassiouny.ahmed.genericmanager.SharedPrefManager;
 
-public class ThanksActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
-    WebView webView;
-    ProgressBar pbProgress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_thanks);
-        webView = findViewById(R.id.webview);
-        pbProgress = findViewById(R.id.pbProgress);
-
-        webView.getSettings().setLoadsImagesAutomatically(true);
-        webView.getSettings().setJavaScriptEnabled(true);
-        //webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        webView.loadUrl("");
-        webView.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress) {
-                //progressBar.setProgress(progress);
-                if (progress == 100) {
-                    pbProgress.setVisibility(View.GONE);
-                }
-            }
-        });
-       /* findViewById(R.id.out).setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_home);
+        findViewById(R.id.out).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
@@ -44,7 +24,7 @@ public class ThanksActivity extends AppCompatActivity {
                         switch (which){
                             case DialogInterface.BUTTON_POSITIVE:
                                 SharedPrefManager.clearSharedPref();
-                                startActivity(new Intent(ThanksActivity.this,LoginActivity.class));
+                                startActivity(new Intent(HomeActivity.this,LoginActivity.class));
                                 finish();
                                 break;
 
@@ -55,11 +35,23 @@ public class ThanksActivity extends AppCompatActivity {
                     }
                 };
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(ThanksActivity.this);
+                AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
                 builder.setMessage(getString(R.string.are_you_sure)).setPositiveButton(getString(R.string.yes), dialogClickListener)
                         .setNegativeButton(getString(R.string.no), dialogClickListener).show();
             }
-        });*/
+        });
+        findViewById(R.id.btn_main).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this,ThanksActivity.class));
+            }
+        });
 
+        findViewById(R.id.btn_add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this,ProductActivity.class));
+            }
+        });
     }
 }
